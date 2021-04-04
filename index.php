f91bec3c5905645b9e8a6879878954edb4d1635b
zile028@gmail.com <zile028@gmail.com>
2021-04-04 18:31:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+
+    <!-- Animate JS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+
+    <title>It Fusion Sourcing</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+
+</body>
+
+</html>
